706251c1300aad4a95900cda0f6278a368317e13
Javidh <98268633+mohammedjavidh17@users.noreply.github.com>
2022-04-02 22:23:28 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -2,18 +2,19 @@ import face_recognition
 import numpy as np
 import cv2
 
-def Train(images):
-    encodedList = []
+def Train(images):                  #images arg (List of location of imageData of students)
+    encodedList = []  
     for x in images:
         img = face_recognition.load_image_file(x)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encd = face_recognition.face_encodings(img)[0]
-        encodedList.append(encd)
-    return encodedList
+        encodedList.append(encd)    #encode the image and add it to a list
+    return encodedList              #returns the encoded list
 
-def Test(imgLoc, trainedList):
+def Test(imgLoc, trainedList):                      
     img = face_recognition.load_image_file(imgLoc)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     encoed = face_recognition.face_encodings(img)[0]
     results = face_recognition.compare_faces(trainedList, encoed)
     return results
+
