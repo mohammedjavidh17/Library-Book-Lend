6188337cd6746295888d403fa9e10d15d9dd554c
Javidh <98268633+mohammedjavidh17@users.noreply.github.com>
2022-04-03 12:20:56 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,5 +1,9 @@
 import face_recognition
 import numpy as np
+from tkinter import *
+import pandas as pd
+import random, string
+from tkinter import ttk
 import cv2
 
 def Train(images):                  #images arg (List of location of imageData of students)
@@ -19,3 +23,59 @@ def Test(img, trainedList):
         return results
     except:
         return []
+
+def TableDel(tr):
+    for x in tr.get_children():
+        tr.delete(x)
+def TableFetch(tr, clm):
+    lst = []
+    for x in tr.get_children():
+        lst.append(tr.item(x)['values'])
+    return pd.DataFrame(lst, columns=clm)
+def TableApp(dt, tr, uniq):   #data, tree, unique append (True/ False)
+    lst = []
+    lst1 = []
+    dtc = dt
+    if uniq:
+        for x in tr.get_children():
+            lst.append(list(tr.item(x)['values']))
+
+        for x in range(dt.shape[0]):
+            if list(dt.iloc[x]) not in lst:
+                lst1.append(list(dt.iloc[x]))
+        dtc = pd.DataFrame(lst1)
+        
+
+    for x in range(dtc.shape[0]):
+        # generating random strings 
+
+        print("hell")
+        res = ''.join(random.choices(string.ascii_uppercase +string.digits, k = 10))
+        tr.insert(parent='',index='end', iid=str(res), values=tuple(dtc.iloc[x]), text='')
+        break           
+def TableDis(dt, column_name, max_ht, rt, app, wid):    #Tree view creater
+    
+    w = ttk.Scrollbar(rt)
+    w.pack(side=RIGHT, fill = 'y')
+    tr = ttk.Treeview(rt, yscrollcommand=w.set)
+    tr['columns'] = column_name
+    tr.column('#0', minwidth=0, width=0)
+    for x in range(len(column_name)):
+        id = '#'+str(x+1)
+        tr.column(id, anchor=W, width=wid)
+    for x in column_name:
+        tr.heading(x, text = x, anchor=W)
+    tr['height'] = max_ht
+    if app:
+        for x in range(dt.shape[0]):
+            # generating random strings 
+            while(True):
+                try:
+                    res = ''.join(random.choices(string.ascii_uppercase +string.digits, k = 10))
+                    tr.insert(parent='',index='end', iid=str(res), values=tuple(dt.iloc[x]), text='')
+                    break
+                except:
+                    pass
+    tr.pack()
+    w.config(command=tr.yview)
+    return tr
